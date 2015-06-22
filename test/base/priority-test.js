2958d5ad208d78f0f4dec2949593dff2c4a9f1dd
Fedor Indutny <fedor@indutny.com>
2015-06-21 21:05:15 -0700

    priority: node removal

--- test/base/priority-test.js
+++ test/base/priority-test.js
@@ -19,7 +19,7 @@ describe('Stream Priority tree', function() {
     tree.add({ id: 3, parent: 0, weight: 2 });
     tree.add({ id: 4, parent: 1, weight: 2 });
 
-    // Results sh
+    // Results
     assert.deepEqual([ 1, 2, 3, 4, 5 ].map(function(id) {
       return tree.get(id).priority;
     }), [ 0.25, 0.5, 0.25, 0.125, 0.125 ]);
@@ -33,5 +33,28 @@ describe('Stream Priority tree', function() {
     var node = tree.add({ id: 1, parent: 3 });
     assert.equal(node.parent.id, 0);
     assert.equal(node.weight, tree.defaultWeight);
+  });
+
+  it('should remove empty node', function() {
+    var node = tree.add({ id: 1, parent: 0, weight: 1 });
+    assert(tree.get(1) !== undefined);
+    node.remove();
+    assert(tree.get(1) === undefined);
+  });
+
+  it('should move children to parent node on removal', function() {
+    // Tree from the first test
+    var one = tree.add({ id: 1, parent: 0, weight: 2 });
+    tree.add({ id: 5, parent: 1, weight: 2 });
+    tree.add({ id: 2, parent: 0, weight: 4 });
+    tree.add({ id: 3, parent: 0, weight: 2 });
+    tree.add({ id: 4, parent: 1, weight: 2 });
+
+    one.remove();
+    assert(tree.get(1) === undefined);
+
+    assert.deepEqual([ 2, 3, 4, 5 ].map(function(id) {
+      return tree.get(id).priority;
+    }), [ 0.4, 0.2, 0.2, 0.2 ]);
   });
 });
