a61578d3d6aa55e7d9f89e96a52451cb5ef56790
Fedor Indutny <fedor@indutny.com>
2015-06-22 21:32:41 -0700

    priority: finish up

--- test/base/priority-test.js
+++ test/base/priority-test.js
@@ -5,7 +5,7 @@ var transport = require('../../');
 describe('Stream Priority tree', function() {
   var tree;
   beforeEach(function() {
-    tree = transport.Priority.create();
+    tree = transport.Priority.create({});
   });
 
   it('should create basic tree', function() {
@@ -38,8 +38,10 @@ describe('Stream Priority tree', function() {
   it('should remove empty node', function() {
     var node = tree.add({ id: 1, parent: 0, weight: 1 });
     assert(tree.get(1) !== undefined);
+    assert.equal(tree.count, 2);
     node.remove();
     assert(tree.get(1) === undefined);
+    assert.equal(tree.count, 1);
   });
 
   it('should move children to parent node on removal', function() {
@@ -50,11 +52,72 @@ describe('Stream Priority tree', function() {
     tree.add({ id: 3, parent: 0, weight: 2 });
     tree.add({ id: 4, parent: 1, weight: 2 });
 
+    assert.equal(tree.count, 6);
     one.remove();
     assert(tree.get(1) === undefined);
+    assert.equal(tree.count, 5);
 
     assert.deepEqual([ 2, 3, 4, 5 ].map(function(id) {
       return tree.get(id).priority;
     }), [ 0.4, 0.2, 0.2, 0.2 ]);
+  });
+
+  it('should move children on exclusive addition', function() {
+    //            0
+    //          /   \
+    //        1       2
+    //      / | \
+    //    3   4    5
+    tree.add({ id: 1, parent: 0, weight: 2 });
+    tree.add({ id: 2, parent: 0, weight: 2 });
+    tree.add({ id: 3, parent: 1, weight: 4 });
+    tree.add({ id: 4, parent: 1, weight: 2 });
+    tree.add({ id: 5, parent: 1, weight: 2 });
+
+    assert.deepEqual([ 1, 2, 3, 4, 5 ].map(function(id) {
+      return tree.get(id).priority;
+    }), [ 0.5, 0.5, 0.25, 0.125, 0.125 ]);
+
+    //            0
+    //          /   \
+    //        1       2
+    //        |
+    //        6
+    //      / | \
+    //    3   4    5
+    tree.add({ id: 6, parent: 1, exclusive: true, weight: 2 });
+
+    assert.deepEqual([ 1, 2, 3, 4, 5, 6 ].map(function(id) {
+      return tree.get(id).priority;
+    }), [ 0.5, 0.5, 0.25, 0.125, 0.125, 0.5 ]);
+  });
+
+  it('should remove excessive nodes on hitting maximum', function() {
+    tree = transport.Priority.create({
+      maxCount: 6
+    });
+
+    //            0
+    //          /   \
+    //        1       2
+    //      / | \
+    //    3   4    5
+    tree.add({ id: 1, parent: 0, weight: 2 });
+    tree.add({ id: 2, parent: 0, weight: 2 });
+    tree.add({ id: 3, parent: 1, weight: 4 });
+    tree.add({ id: 4, parent: 1, weight: 2 });
+    tree.add({ id: 5, parent: 1, weight: 2 });
+
+    //            0
+    //          /   \
+    //        6       2
+    //      / | \
+    //    3   4    5
+    tree.add({ id: 6, parent: 1, exclusive: true, weight: 2 });
+
+    assert.equal(tree.get(1), undefined);
+    assert.deepEqual([ 2, 3, 4, 5, 6 ].map(function(id) {
+      return tree.get(id).priority;
+    }), [ 0.5, 0.25, 0.125, 0.125, 0.5 ]);
   });
 });
